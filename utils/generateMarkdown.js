b0049d8d592fa4636dcca0906d02c0b08e8d330a
timothymateo <timothymurphy2022@u.northwestern.edu>
2022-06-18 19:59:42 -0500

    fixing rendering issues with the license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,55 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// TODO: Create a function that returns the license link
-function renderLicenseLink(license) {
-    if (license === 'MIT') {
-      licenseLink = `[MIT](https://opensource.org/licenses/MIT)`;
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license !== 'None') {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
     }
-    if (license === 'GPLv3') {
-      licenseLink = `[GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
+    return '';
+  }
+  
+  // TODO: Create a function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license !== 'None') {
+      return `* [License](#license)`
+    } else {
+      return '';
     }
-    if (license === 'Apache--2.0') {
-      licenseLink = `[Apache-2.0](https://opensource.org/licenses/Apache-2.0)`;
-    }
-    if (license === 'ISC') {
-      licenseLink = `[ISC](https://opensource.org/licenses/ISC)`;
-    }
+  }
+  
+  // TODO: Create a function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license !== 'None') {
+      return `## License 
+      This project is licensed under the ${license} license.`
+    } 
+    return '';
   }
   
   // TODO: Create a function to generate markdown for README
-  function generateMarkdown(answers) {
-    renderLicenseLink(answers.license);
-    console.log(licenseLink);
-    return `# ${answers.title} 
-    ![license-badge](https://img.shields.io/badge/license-${answers.license}-blue)
+  function generateMarkdown(data) {
+    return `# ${data.title}
+    ${renderLicenseBadge(data.license)}
+    ## Description 
+    ${data.description}
     ## Table of Contents
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing-Guidelines](#contributing-guidelines)
-    - [Tests](#tests)
-    - [License](#license)
-    - [Questions](#questions)
-  
-    ## Description
-    ${answers.description}
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    * [Contributing](#contribute)
+    * [Tests](#test)
+    * [Questions](#questions)
     ## Installation
-    ${answers.installation}
+    ${data.installation}
     ## Usage
-    ${answers.usage}
-    ## Contributing Guidelines
-    ${answers.contributing}
+    ${data.usage}
+    ${renderLicenseSection(data.license)}
+    ## Contributing
+    ${data.contribute}
     ## Tests
-    ${answers.tests}
-    ## License
-    ${answers.license} License
-    Copyright (c) 2022 ${answers.username}
-    Licensed under the ${licenseLink} License.
+    ${data.tests}
     ## Questions
-   * GitHub Profile - https://github.com/${answers.username}
-   * Email me at ${answers.email}
-    
+    ${data.questions}
+    [GitHub](github.com/${data.github})
+    [Email](mailto:${data.email})
   `;
   }
-  
-  module.exports = generateMarkdown;
-  
