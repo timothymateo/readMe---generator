d9414aeb4517b762f4ee9026925a8343cbcee8bd
timothymateo <timothymurphy2022@u.northwestern.edu>
2022-06-18 20:22:55 -0500

    trying to fix issues with the generateMarkdown.js file

---
+++ README.md
@@ -0,0 +1,31 @@
+# ReadMe This  
+    
+    ![license-badge](https://img.shields.io/badge/license-MIT-blue)
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing-Guidelines](#contributing-guidelines)
+    - [Tests](#tests)
+    - [License](#license)
+    - [Questions](#questions)
+  
+    ## Description
+    generates a file
+    ## Installation
+    download package
+    ## Usage
+    via terminal on mac
+    ## Contributing Guidelines
+    adding stuff
+    ## Tests
+    run a test
+    ## License
+    MIT License
+    Copyright (c) 2022 timothymateo
+    Licensed under the [MIT](https://opensource.org/licenses/MIT) License.
+    ## Questions
+   * GitHub Profile - https://github.com/timothymateo
+   * Email me at timoteo@gmail.com
+    
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,57 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    if (license !== 'None') {
-      return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+// TODO: Create a function that returns the license link
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+      licenseLink = `[MIT](https://opensource.org/licenses/MIT)`;
     }
-    return '';
-  }
-  
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) {
-    if (license !== 'None') {
-      return `* [License](#license)`
-    } else {
-      return '';
+    if (license === 'GPLv3') {
+      licenseLink = `[GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
     }
-  }
-  
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {
-    if (license !== 'None') {
-      return `## License 
-      This project is licensed under the ${license} license.`
-    } 
-    return '';
+    if (license === 'Apache--2.0') {
+      licenseLink = `[Apache-2.0](https://opensource.org/licenses/Apache-2.0)`;
+    }
+    if (license === 'ISC') {
+      licenseLink = `[ISC](https://opensource.org/licenses/ISC)`;
+    }
   }
   
   // TODO: Create a function to generate markdown for README
-  function generateMarkdown(data) {
-    return `# ${data.title}
-    ${renderLicenseBadge(data.license)}
-    ## Description 
-    ${data.description}
+  function generateMarkdown(answers) {
+    renderLicenseLink(answers.license);
+    console.log(licenseLink);
+    return `# ${answers.title} 
+    
+    ![license-badge](https://img.shields.io/badge/license-${answers.license}-blue)
     ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    ${renderLicenseLink(data.license)}
-    * [Contributing](#contribute)
-    * [Tests](#test)
-    * [Questions](#questions)
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing-Guidelines](#contributing-guidelines)
+    - [Tests](#tests)
+    - [License](#license)
+    - [Questions](#questions)
+  
+    ## Description
+    ${answers.description}
     ## Installation
-    ${data.installation}
+    ${answers.installation}
     ## Usage
-    ${data.usage}
-    ${renderLicenseSection(data.license)}
-    ## Contributing
-    ${data.contribute}
+    ${answers.usage}
+    ## Contributing Guidelines
+    ${answers.contributing}
     ## Tests
-    ${data.tests}
+    ${answers.tests}
+    ## License
+    ${answers.license} License
+    Copyright (c) 2022 ${answers.username}
+    Licensed under the ${licenseLink} License.
     ## Questions
-    ${data.questions}
-    [GitHub](github.com/${data.github})
-    [Email](mailto:${data.email})
+   * GitHub Profile - https://github.com/${answers.username}
+   * Email me at ${answers.email}
+    
   `;
   }
+  
+  module.exports = generateMarkdown;
+  
